e8bcd7176ce5a0040d83b9bef2f9f8956331960c
George <gbglegacy@gmail.com>
2016-08-04 21:29:00 +0000

    added paginations to views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,8 @@ class ArticlesController < ApplicationController
     before_action :set_article, only: [:edit, :update, :show, :destroy]
     
     def index
-        @articles = Article.all #gets all the articles in the database
+       # @articles = Article.all #gets all the articles in the database
+       @articles = Article.paginate(page: params[:page], per_page: 5)
     end
     
     def new
